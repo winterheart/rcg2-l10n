98b6ad20e9c6a1532ae2178171de0220a739e0d2
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2023-01-17 02:47:50 +0300

    Refactor creating PO/POT files
    Force creating POT files as they may be needed in translation flow.

--- scripts/lib/rcg2_l10n.py
+++ scripts/lib/rcg2_l10n.py
@@ -44,8 +44,8 @@ LANG_KEY = "Key"
 METADATA_ENTRY = {
     'Project-Id-Version': '1.0',
     'Report-Msgid-Bugs-To': 'you@example.com',
-    'POT-Creation-Date': datetime.now().isoformat(" ", "seconds"),
-    'PO-Revision-Date': datetime.now().isoformat(" ", "seconds"),
+    'POT-Creation-Date': datetime.now().strftime("%Y-%m-%d %H:%M%z"),
+    'PO-Revision-Date': datetime.now().strftime("%Y-%m-%d %H:%M%z"),
     'Last-Translator': 'you <you@example.com>',
     'Language-Team': '',
     'MIME-Version': '1.0',
@@ -60,6 +60,7 @@ class Rcg2Translation:
         Init class for handling RCG_LocalizationData.json
         :param csv_path: Path to CSV file (UTF-8, ";" as delimiter)
         """
+        self.logger = logging.getLogger("RCG2")
         with open(csv_path, "r", newline="", encoding="utf-8") as read_file:
             csv_content = csv.DictReader(read_file, delimiter=";")
             self.content = list(csv_content)
@@ -95,43 +96,6 @@ class Rcg2Translation:
                 writer.writerow(row)
         return
 
-    def generate_pot(self, csv_root_key):
-        """
-        Generate and return POT file object
-        :param csv_root_key: JSON key from RcgJsonKeys class
-        :return: POT file object
-        """
-        pot = POFile(check_for_duplicates=True)
-        pot.metadata = METADATA_ENTRY
-        pot.metadata_is_fuzzy = 1
-
-        for entry in self.content[csv_root_key.value]:
-            if entry[Rcg2Languages.LANG_ENGLISH.value["key"]] != "":
-                po_entry = POEntry(
-                    msgctxt=entry[LANG_KEY],
-                    msgid=entry[Rcg2Languages.LANG_ENGLISH.value["key"]],
-                )
-                try:
-                    pot.append(po_entry)
-                except ValueError:
-                    logging.debug("Entry {} already exists, skipping...".format(entry[LANG_KEY]))
-
-        return pot
-
-    def save_pot(self, path, csv_root_key):
-        """
-        Save Gettext POT file from JSON root key into path
-        :param path: Directory path save to
-        :param csv_root_key: JSON key from RcgJsonKeys class
-        :return:
-        """
-        pot = self.generate_pot(csv_root_key)
-        if not exists(path):
-            makedirs(path)
-        pot.save(join(path, csv_root_key.value + ".pot"))
-
-        return
-
     def load_po(self, path, csv_root_key, lang):
         """
         Load content of Gettext PO file (from "path/lang/json_root_key.po") into already loaded JSON content
@@ -140,13 +104,6 @@ class Rcg2Translation:
         :param lang: Language. Should be in RcgLanguages class
         :return:
         """
-
-        #if csv_root_key.value not in self.content:
-        #    logging.warning("WARNING: {} CSV entry is empty!".format(csv_root_key.value))
-        #    return
-
-#        temp_json = dict([])
-#        temp_json.update({csv_root_key.value: []})
 
         language = next(name.value["key"] for name in Rcg2Languages if name.value["iso_code"] == lang)
 
@@ -164,7 +121,7 @@ class Rcg2Translation:
                         # Add entry if there is no entry at all
                         csv_entry[language] = entry.msgid
         else:
-            logging.warning("ERROR: '{}' is not exists! Skipping.".format(po_file))
+            self.logger.warning("ERROR: '{}' is not exists! Skipping.".format(po_file))
 
         return
 
@@ -183,39 +140,69 @@ class Rcg2Translation:
                 self.po_file = pot_file
                 self.create = create
 
+        # Create POT files
+        pot_files = dict()
+        pot_path = join(path, "pot")
+        if not exists(pot_path):
+            makedirs(pot_path)
+
+        for i in Rcg2CsvKeys:
+            pot_files[i.value] = PotFileEntry(join(pot_path, i.value + ".pot"), POFile(check_for_duplicates=True),
+                                              not exists(join(pot_path, i.value + ".pot")))
+            pot_files[i.value].po_file.metadata = METADATA_ENTRY
+            pot_files[i.value].po_file.metadata_is_fuzzy = 1
+
+        for row in self.content:
+            (category, key) = row[LANG_KEY].split("/")
+            if category in pot_files.keys() and key != "" and row[Rcg2Languages.LANG_ENGLISH.value["key"]] != "":
+                po_entry = POEntry(
+                    msgctxt=key,
+                    msgid=row[Rcg2Languages.LANG_ENGLISH.value["key"]],
+                )
+                # Add translator's comment with japanese string
+                po_entry.comment = row[Rcg2Languages.LANG_JAPANESE.value["key"]]
+                try:
+                    pot_files[category].po_file.append(po_entry)
+                except ValueError:
+                    self.logger.debug("Entry {} already exists, skipping...".format(row[LANG_KEY]))
+        # We ready to dump POT files into FS
+        for i in Rcg2CsvKeys:
+            self.logger.info("Saving POT-file {}".format(pot_files[i.value].path))
+            pot_files[i.value].po_file.save(pot_files[i.value].path)
+
         for lang_code in languages:
             language = next(name.value["key"] for name in Rcg2Languages if name.value["iso_code"] == lang_code)
             save_path = join(path, lang_code)
             if not exists(save_path):
                 makedirs(save_path)
 
-            pot_files = dict()
             for i in Rcg2CsvKeys:
-                pot_files[i.value] = PotFileEntry(join(save_path, i.value + ".po"), POFile(check_for_duplicates=True),
-                                                  not exists(join(save_path, i.value + ".po")))
-                pot_files[i.value].po_file.metadata = METADATA_ENTRY
-
-            for row in self.content:
-                (category, key) = row[LANG_KEY].split("/")
-                if category in pot_files.keys() and key != "" and row[Rcg2Languages.LANG_ENGLISH.value["key"]] != "":
-                    po_entry = POEntry(
-                        msgctxt=key,
-                        msgid=row[Rcg2Languages.LANG_ENGLISH.value["key"]],
-                    )
-                    if language in row.keys() and row[language] != "" and pot_files[category].create:
-                        po_entry.msgstr = row[language]
-                        po_entry.flags.append("fuzzy")
-                    try:
-                        pot_files[category].po_file.append(po_entry)
-                    except ValueError:
-                        logging.debug("Entry {} already exists, skipping...".format(row[LANG_KEY]))
-
-            for file in pot_files.values():
-                if file.create:
-                    file.po_file.save(file.path)
+                po_path = join(save_path, i.value + ".po")
+                po_file = pot_files[i.value].po_file
+                for entry in po_file:
+                    target_str = self.find_csv_entry(language, "/".join([i.value, entry.msgctxt]))
+                    if target_str != "":
+                        entry.msgstr = target_str
+                        entry.flags.append("fuzzy")
+                if exists(po_path):
+                    self.logger.info("Merging PO-file {}".format(po_path))
+                    po = pofile(po_path)
+                    po.merge(po_file)
+                    po.save(po_path)
                 else:
-                    po = pofile(file.path)
-                    po.merge(file.po_file)
-                    po.save(file.path)
-
+                    self.logger.info("Saving PO-file {}".format(po_path))
+                    po_file.save(po_path)
         return
+
+    def find_csv_entry(self, column, input):
+        """
+        Lookup entry in loaded CSV file
+        :param column: Where to search
+        :param input: Reference input in English
+        :return: Translated string or empty string if no such entry
+        """
+        for row in self.content:
+            if row[LANG_KEY] == input:
+                return row[column]
+        self.logger.warning("Entry {} ({}) not found in CSV".format(input, column))
+        return ""
