81083dfc8241e1bf2b8615e0a3ea8ddb2d0f7d9e
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2023-01-28 05:06:41 +0300

    Импорт ассетов Unity

---
+++ unity/rcg2-l10n-assets/Assets/Scripts/CreateAssetBundle.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        string assetName = "rcg2-l10n.assetbundle";
+        Directory.Delete(assetBundleDirectory, true);
+        Directory.CreateDirectory(assetBundleDirectory);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+                                        BuildAssetBundleOptions.ChunkBasedCompression,
+                                        BuildTarget.StandaloneWindows);
+
+        File.Copy(Path.Combine(assetBundleDirectory, assetName), Path.Combine("..", assetName), true);
+    }
+}
+#endif
